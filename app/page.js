eb2265a1a3c7b9088a1e6482dea880e944442b9e
Kanish Kumar <kanishkumar11@outlook.in>
2023-04-19 19:38:07 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,36 @@
+import { Footer, Navbar } from '../components';
+import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+
+const Page = () => (
+  <div className='bg-primary-black overflow-hidden'>
+    <Navbar />
+    <Hero />
+    <div className='relative'> 
+      <Explore />
+
+      <div className='gradient-03 z-0' />
+      <About />
+
+    </div>
+    {/* 
+
+    </div>
+    <div className='relative'>
+      <GetStarted />
+      <div className='gradient-04 z-0' />
+      <WhatsNew />
+    </div>
+    <World />
+
+    <div className='relative'>
+      <Insights />
+
+    <div className='gradient-04 z-0' />
+      <Feedback />
+
+    </div>
+    <Footer /> */}
+  </div> 
+);
+
+export default Page;
