e638e351dd6ced0fa08af619e269bbf64e77a115
Kanish Kumar <kanishkumar11@outlook.in>
2023-04-19 19:39:10 +0530

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,123 @@
+import { people01, people02, people03 } from "./";
+
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    // imgUrl: '/planet-01.png',
+    title: 'Video Editing',
+  },
+  {
+    id: 'world-2',
+    // imgUrl: '/planet-02.png',
+    title: 'Graphic Designing',
+  },
+  {
+    id: 'world-3',
+    // imgUrl: '/planet-03.png',
+    title: 'Content Creation',
+  },
+  {
+    id: 'world-4',
+    // imgUrl: '/planet-04.png',
+    title: 'Social Media Management',
+  },
+  {
+    id: 'world-5',
+    // imgUrl: '/planet-05.png',
+    title: 'Web Development',
+  },
+  {
+    id: 'world-5',
+    // imgUrl: '/planet-05.png',
+    title: 'App Development',
+  },
+  {
+    id: 'world-5',
+    // imgUrl: '/planet-05.png',
+    title: 'Web Design',
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:"I've been using this agency for all of my marketing and design needs for the past year, and they never disappoint. Their team is highly skilled and always delivers top-quality work. From video editing to web development, they truly offer complete solutions for any business's digital needs.",
+    name: "Prashant Dhavan",
+    // title: "Founder & Leader",
+    img: './people01.jpg',
+  },
+  {
+    id: "feedback-2",
+    content:"If you're looking for a reliable agency that can handle all of your digital needs, then look no further. This agency provides top-notch services in video editing, web development, digital marketing, and graphic design. Their team is highly skilled and always delivers high-quality work on time and on budget. I would highly recommend them to anyone.",
+    name: "Steve Mark",
+    // title: "Founder & Leader",
+    img: 'people02.png',
+  },
+  {
+    id: "feedback-3",
+    content:"This agency is a one-stop-shop for all things digital. Their team is highly skilled and always goes above and beyond to ensure that their clients are satisfied. They provide excellent services in video editing, web development, and graphic design. I highly recommend them to anyone looking for reliable and professional digital solutions.",
+    name: "Shashwant",
+    // title: "Founder & Leader",
+    img: './people03.jpg',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  '<a href="tel:9425115731">9425115731</a><a>9425115731</a>',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
+    subtitle:
+        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 tips to easily master the madness of the Metaverse',
+    subtitle:
+        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'With one platform you can explore the whole world virtually',
+    subtitle:
+        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+  },
+];
+
+export const socials = [
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+    link: 'https://www.linkedin.com/company/the-urban-cut-studios1/',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+    link: 'https://instagram.com/theurbancutstudiosofficial',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+    link: 'https://www.facebook.com/profile.php?id=100091894932866',
+  },
+];
