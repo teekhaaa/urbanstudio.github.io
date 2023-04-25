59395e968f8d2453251ccce1dfbce14057017f0c
teekhaaa <131258001+teekhaaa@users.noreply.github.com>
2023-04-25 12:03:35 +0530

    fixed

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -7,7 +7,7 @@ const Testimonials = () => (
     <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1] ml-[200px]">
-      <h2 className={styles.heading2}>
+      <h2 className={`${styles.heading2} ml-[10px]`}>
         What People are <br className="sm:block hidden" /> saying about us
       </h2>
       <div className="w-full md:mt-0 mt-6">
