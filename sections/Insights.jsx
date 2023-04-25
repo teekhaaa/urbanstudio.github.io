3c98a354a1ff3f54859c8212d2abf2acfc77be0c
teekhaaa <131258001+teekhaaa@users.noreply.github.com>
2023-04-25 11:44:07 +0530

    fixes

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -18,22 +18,22 @@ const Insights = () => (
     <TitleText title={<>Our Expertise</>} textStyles='text-center' />
       <div className={`${styles.yPaddings} flex`}>
         <ul className='flex gap-10 flex-wrap'>
-          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] rounded-2xl'>Graphic Designing</li>
-          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] rounded-2xl'>Wedding Invites</li>
-          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] rounded-2xl'>LogoDesign</li>
-          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] rounded-2xl'>V.F.X.</li>
-          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] rounded-2xl'>2D Animation</li>
-          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] rounded-2xl'>3D Animation</li>
-          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] rounded-2xl'>3D Models</li>
-          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] rounded-2xl'>Composition</li>
-          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] rounded-2xl'>Package Design</li>
-          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] rounded-2xl'>Video Editing</li>
-          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] rounded-2xl'>Digital Marketing</li>
-          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] rounded-2xl'>Socail Media Marketing</li>
-          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] rounded-2xl'>UI/UX Design</li>
-          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] rounded-2xl'>Video Animation</li>
-          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] rounded-2xl'>Wedding Video Editing</li>
-          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] rounded-2xl'>App Development</li>
+          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] md:text-[22px] rounded-2xl'>Graphic Designing</li>
+          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] md:text-[22px] rounded-2xl'>Wedding Invites</li>
+          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] md:text-[22px] rounded-2xl'>LogoDesign</li>
+          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] md:text-[22px] rounded-2xl'>V.F.X.</li>
+          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] md:text-[22px] rounded-2xl'>2D Animation</li>
+          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] md:text-[22px] rounded-2xl'>3D Animation</li>
+          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] md:text-[22px] rounded-2xl'>3D Models</li>
+          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] md:text-[22px] rounded-2xl'>Composition</li>
+          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] md:text-[22px] rounded-2xl'>Package Design</li>
+          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] md:text-[22px] rounded-2xl'>Video Editing</li>
+          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] md:text-[22px] rounded-2xl'>Digital Marketing</li>
+          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] md:text-[22px] rounded-2xl'>Socail Media Marketing</li>
+          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] md:text-[22px] rounded-2xl'>UI/UX Design</li>
+          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] md:text-[22px] rounded-2xl'>Video Animation</li>
+          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] md:text-[22px] rounded-2xl'>Wedding Video Editing</li>
+          <li className='text-[#117ADC] gap-14 bg-[#D4DAE1] font-semibold md:p-[25px] p-[20px] px-[10px] md:px-[60px] text-[18px] md:text-[22px] rounded-2xl'>App Development</li>
           
 
         </ul>
